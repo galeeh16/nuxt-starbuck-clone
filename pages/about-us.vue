3c349ada521179e11350be16eca2b03eaa3685d2
Galih Anggoro Jati <anggorogalih8@gmail.com>
2023-04-06 15:09:42 +0700

    add page starbuck rewards and about

---
+++ pages/about-us.vue
@@ -0,0 +1,34 @@
+<template>
+  <div :class="style.container" class="py-10 flex w-full gap-x-8">
+    <div class="flex-1">
+      <h1 class="text-[3rem] text-[400] mt-1 mb-3">About Us</h1>
+
+      <h2 class="text-[1.5rem]">It happens millions of times each week - a customer receives a drink from a Starbucks
+        barista - but each
+        interaction is unique..</h2>
+
+      <img src="https://www.starbucks.co.id/storage/image/temporary/summernote_image_1648202516.jpg" alt="about us"
+        class="mt-4">
+
+      <p class="mt-12 leading-[28px]">It's just a moment in time - just one hand reaching over the counter to present a
+        cup to another outstretched hand.</p>
+
+      <p class="mt-5 leading-[28px]">But it's a connection...</p>
+
+      <p class="mt-5 leading-[28px]">We make sure everything we do honors that connection - from our commitment to <a
+          href="/" class="text-[#2a8a15] hover:text-white hover:bg-[#2a8a15]">the
+          highest quality coffee</a> in the world, to the way we engage with our customers and communities to do business
+        responsibly.</p>
+
+      <p class="mt-5 leading-[28px]">From <a href="/" class="text-[#2a8a15] hover:text-white hover:bg-[#2a8a15]">our
+          beginnings</a> as a single store over forty years ago, in every place that we've
+        been, and every place that we touch, we've tried to make it a little better than we found it.</p>
+    </div>
+
+    <RightSidebar />
+  </div>
+</template>
+
+<script setup>
+import style from '../style'
+</script>
---
+++ pages/starbuck-rewards.vue
@@ -0,0 +1,82 @@
+<template>
+  <div>
+    <img src="https://www.starbucks.co.id/image/card/banner/landing-page-1900x560px-additional-MTR-decoupling-feb23-3.png"
+      alt="rewards">
+  </div>
+
+  <div :class="style.container" class="py-12">
+    <h2 class="text-[#1A3C34] text-[33px] text-center font-bold">GETTING TO KNOW <br /> YOUR STARBUCKS REWARDS JOURNEY
+    </h2>
+    <div class="flex justify-center mt-12">
+      <img src="https://www.starbucks.co.id/image/card/earnstar.png" alt="earnstar">
+    </div>
+    <p class="text-center text-[#1A3C34] px-12 text-[17px] mt-12 leading-[34px]">
+      You will <b>get 1 Star</b> for every multiplication of <b>Rp5.000,-</b> spent <b>using registered Starbucks account
+        balance</b> OR by <b>spending Rp10.000,-</b> using other method of payment <a href="/" class="underline">in
+        selected Starbucks
+        Indonesia stores.</a>
+    </p>
+  </div>
+
+  <div class="bg-[#D7EDDF] py-12">
+    <h2 class="text-[#1A3C34] text-[33px] text-center font-bold">LEVEL UP TO UNLOCK MORE BENEFITS</h2>
+    <div class="flex justify-center mt-2">
+      <img src="https://www.starbucks.co.id/image/card/lvl up 3.png" alt="lvl up 3" class="max-w-[555px]">
+    </div>
+    <p class="text-[#1A3C34] text-center text-[17px] font-semibold mt-8">Collect 300 Stars within a year to get Gold
+      status</p>
+  </div>
+
+  <div class="bg-[#006241] py-12">
+    <div :class="style.container">
+      <h2 class="text-[#FFF] text-[33px] text-center font-bold">REDEEM STARS WITH <br /> CHOICES OF REWARDS</h2>
+
+      <ul class="flex justify-center w-full mt-5">
+        <li class="text-left" v-for="star in stars" :key="star.id">
+          <a class="text-white bg-transparent border-b-[6px] flex items-start align-baseline flex-row py-2 px-4"
+            :class="star.isActive ? 'border-b-[#be872b]' : 'border-b-[#1A3C34]'">
+            <p class="text-[1.25rem] font-bold mb-0">{{ star.number }}</p>
+            <img src="https://www.starbucks.co.id/image/card/star.png" alt="star" class="w-[15px] h-[15px] ml-[4px]">
+          </a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import style from '../style'
+
+const stars = ref([
+  {
+    id: 1,
+    number: 25,
+    isActive: true
+  },
+  {
+    id: 2,
+    number: 50,
+    isActive: false
+  },
+  {
+    id: 3,
+    number: 75,
+    isActive: false
+  },
+  {
+    id: 4,
+    number: 100,
+    isActive: false
+  },
+  {
+    id: 5,
+    number: 150,
+    isActive: false
+  },
+  {
+    id: 6,
+    number: 200,
+    isActive: false
+  },
+])
+</script>
